f350e84d4eb06950cfad9f127c1db67af06f6d0b
gavin <gavinsutton6511@gmail.com>
2023-10-15 17:25:10 +0900

    Finish logic for first working iteration

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
+///////////         CURRENT LOGIC       //////////////
+/*  Numbers are added to the array num2
+    upon an operation button press num2 array is moved into num1 array
+    the second set of input received is moved into num2 array
+    upon pressing equals both arrays join() and the result is displayed as a number
+*/
+
 // display and store into num variables
 const displayNumber = function() {
     let buttonValue = this.textContent;
     displaySection.textContent = displaySection.textContent.concat(buttonValue);
-    num1.push(buttonValue);
+    num2.push(buttonValue);
 }
 
 // display and store into operand variable
@@ -10,56 +17,45 @@ const displayOperator = function() {
     let buttonValue = this.textContent;
     displaySection.textContent = buttonValue;
     operand = buttonValue;
+    if (num1.length === 0) {num1 = num2;} // passes to num1 on first calculation, 2nd operation and on will already have num1 value
+    num2 = []; // reset num2 for new input
 }
 
 // called when equals is pressed
 const calculate = function() {
+    num2 = parseInt(num2.join(""));
+    num1 = parseInt(num1.join(""));
     operate([num1, num2], operand);
+    num1 = Array.from(String(num1)); // need to return to an array for further operations *see bottom for further info
 }
 
-// gives the numbered buttons their onclick functionality
-document.querySelectorAll(".number-button").forEach(function(button){
-    button.addEventListener("click", displayNumber);
-});
-
-// gives the operator buttons their onclick functionality
-document.querySelectorAll(".operator-button").forEach(function(button){
-    button.addEventListener("click", displayOperator);
-});
-
-//gives equals button onclick functionality
-document.getElementById('equals').addEventListener("click", calculate);
-
-let displaySection = document.getElementById("display-numbers");
+// sets display blank
+const clearButton = function() {
+    document.getElementById("display-numbers").textContent = "";
+    num1 = [];
+    num2 = [];
+    operand = undefined;
+}
 
 const add = function(a,b) {
-    let result = a+b;
-    displaySection.textContent = result; // change this to appear in the calculator display area when ready
+    num1 = a+b;
+    displaySection.textContent = num1;
 };
   
 const subtract = function(a,b) {
-    let result = a-b;
-    displaySection.textContent = result;
+    num1 = a-b;
+    displaySection.textContent = num1;
 };
   
 const multiply = function(a,b) {
-    let result = a*b;
-    displaySection.textContent = result;
+    num1 = a*b;
+    displaySection.textContent = num1;
 };
 
 const divide = function(a,b) {
-    let result = a/b;
-    displaySection.textContent = result;
+    num1 = a/b;
+    displaySection.textContent = num1;
 };
-
-let num1 = [];
-let num2 = [];
-let operand; // can be assigned the values 'addition', 'subtraction', 'multiplication', 'division'
-
-// assign the button presses to the correct variable
-const variableAssignment = function() {
-    
-}
 
 const operate = function([num1,num2],operand) {
     switch (operand) {
@@ -80,14 +76,28 @@ const operate = function([num1,num2],operand) {
     }
 };
 
-// sets display blank
-const clearButton = function() {
-    document.getElementById("display-numbers").textContent = "";
-    num1 = [];
-    num2 = [];
-    operand = undefined;
-}
+// gives the numbered buttons their onclick functionality
+document.querySelectorAll(".number-button").forEach(function(button){
+    button.addEventListener("click", displayNumber);
+});
 
-document.getElementById("clear").addEventListener("click", function(){clearButton()});
+// gives the operator buttons their onclick functionality
+document.querySelectorAll(".operator-button").forEach(function(button){
+    button.addEventListener("click", displayOperator);
+});
+
+document.getElementById('equals').addEventListener("click", calculate);
+
+document.getElementById("clear").addEventListener("click", clearButton);
+
+
+let displaySection = document.getElementById("display-numbers");
+let num1 = [];
+let num2 = [];
+let operand; // can be assigned the values 'addition', 'subtraction', 'multiplication', 'division'
+
+
+// * num1 = Array.from(String(num1), Number);
+// * this was the original operation but ran into problems when converting a negative string back into a negative number, the '-' string was assigned NaN in the new array
 
 
